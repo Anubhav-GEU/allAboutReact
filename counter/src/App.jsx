ba2131741aaaec6859fe6655c83643310ccc8c8e
Anubhav-GEU <anubhav860102@gmail.com>
2024-03-26 01:03:01 +0530

    bgChanger

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 function App() {
   let [counter,setCounter] = useState(0);
+  let [color, setColor] = useState("black");
+
   const upVal = () => {
     // method 1
     // setCounter(counter = counter+1);
@@ -29,11 +31,20 @@ function App() {
   }
   return (
     <>
-      <div className="flex bg-black text-white">
+      {/* <div className="flex bg-black text-white">
         <p>Creating counter: {counter}</p>
         <button className="bg-red-500" onClick={upVal}>Up</button>
         <br /><br /><br />
         <button className="bg-blue-500" onClick={downVal}>Down</button>
+      </div> */}
+
+      <div className="m-5 p-5 w-full h-screen " style={{backgroundColor: color}}>
+        <button className="bg-red-500 m-5 rounded-full" onClick={()=>setColor('red')}>Red</button>
+        <button className="bg-blue-500 m-5 rounded-full" onClick={()=>setColor('blue')}>blue</button>
+        <button className="bg-purple-500 m-5 rounded-full" onClick={()=>setColor('purple')}>purple</button>
+        <button className="bg-orange-500 m-5 rounded-full" onClick={()=>setColor('orange')}>Orange</button>
+        <button className="bg-pink-500 m-5 rounded-full"
+        onClick={()=>setColor('pink')}>Pink</button>
       </div>
     </>
   )
